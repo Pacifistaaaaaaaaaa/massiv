ebf6337504c618fa6ef041d3f6bc538677b50a6b
unknown <xfvjykk@yandex.ru>
2022-12-13 21:41:19 +0300

    Сделал задание

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекуться в точке = X: {x}, Y: {y}");
